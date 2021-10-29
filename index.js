804e14d35be7955a7e7cc830dfdf53d6534ab387
Nazim-Habib <c161067@ugrad.iiuc.ac.bd>
2021-10-29 22:47:50 +0600

    added Code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,14 +12,59 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9s2cu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 async function run() {
     try {
         await client.connect();
-        console.log('database connected')
+        const database = client.db("Habib's-Travelling");
+        // create database collection
+        const serviceCollection = database.collection("services");
+        const orderCollection = database.collection("orders");
+        // console.log('database connected')
+
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            // console.log('hit the post api', breakfast);
+            const result = await serviceCollection.insertOne(service);
+            // console.log(result);
+            res.json(result)
+        });
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const service = await cursor.toArray();
+            res.send(service);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        });
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            // console.log('hit the post api', breakfast);
+            const result = await orderCollection.insertOne(order);
+            // console.log(result);
+            res.json(result)
+        });
+
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const order = await cursor.toArray();
+            res.send(order);
+        });
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
 
     }
     finally {
